4bba1a54d1e745fbfe1844957322f597b8a53df9
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-01-15 11:32:43 -0600

    dom event

---
+++ js/DOM-event-handling.js
@@ -0,0 +1,29 @@
+let btn1 = document.getElementById('one');
+let btn2 = document. getElementById('two');
+let btn3 = document.getElementById('three');
+
+let handleClick = function(event){
+    console.log("did you click me ?");
+}
+
+btn2.addEventListener('click', handleClick);
+btn2.addEventListener('click',function(){
+    console.log("another handler has been added")
+});
+
+btn2.removeEventListener('click',handleClick);
+
+//let addDiv= document.createElement('div')
+ //use appendChild
+//document.body.appendChild(div);
+
+let btn4 = document.getElementById('push')
+let div= document.getElementById('area')
+
+btn4.addEventListener('click', function(){
+    console.log("creating div")
+    const newDiv= document.createElement("div")
+    div.appendChild(newDiv)
+})
+
+
